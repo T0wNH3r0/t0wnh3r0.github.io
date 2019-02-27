ac31ab37db2fb7330ec5103e0ed8774c7437faf8
Kevin <kevinlaws.dev@gmail.com>
2019-02-27 10:44:16 -0500

    more changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
+import Icon from '@material-ui/core/Icon';
 export default class About extends Component {
   render() {
     let resumeData = this.props.resumeData;
@@ -12,14 +14,34 @@ export default class About extends Component {
           <div className="nine columns main-col">
             <h2>About Me</h2>
             <p>{resumeData.aboutme}</p>
-            
+
             <div className="row">
               <div className="columns contact-details">
                 <h2>Contact Details</h2>
-                <p className="address">
-                  <span>{resumeData.email}</span>
+                <span>Reach me on social media:</span>
+                <ul className="social">
+                  {resumeData.socialLinks &&
+                    resumeData.socialLinks.map(item => {
+                      return (
+                        <li key={item.name}>
+                          <a
+                            href={item.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <Icon
+                              className={classNames(item.className)}
+                              style={{ fontSize: '2em' }}
+                            />
+                          </a>
+                        </li>
+                      );
+                    })}
+                </ul>
+             
+                  <span>or send me an email: {resumeData.email}</span>
                   <br />
-                </p>
+               
               </div>
             </div>
           </div>
